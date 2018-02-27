9d0ec86d4422df99b989455e8977b6c18d142305
Nicola <nicregattieri@gmail.com>
2018-02-27 20:44:21 -0300

    added galery items and changed social colors

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,18 +1,15 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba') ;
+sass = require('gulp-sass'),
+node_path = '/node_modules/aos/dist' ;
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer, hexrgba]))
-    .on('error', function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
+  return gulp.src('./app/assets/styles/styles.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app/temp/styles'));
+    
+    
 });
+
+// vendors
+
+
